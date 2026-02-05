d16e93ee96e4090c0dfc882d67137e93376294b6
Iypan-admin <writetoteachersview@gmail.com>
2026-02-05 12:52:20 +0530

    Change server listen address to '0.0.0.0'

--- server.js
+++ server.js
@@ -168,6 +168,6 @@ app.post("/admin/download-registrations", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
